66baaadfb3b6aac9741eb42b807fb540e3c58b1f
Edinorig <super-ddred@ukr.net>
2021-09-19 17:18:28 +0200

    Vigener 0.3

--- Vysotskyy_Denys_Genevre/src/vigenere/Vigenere.java
+++ Vysotskyy_Denys_Genevre/src/vigenere/Vigenere.java
@@ -5,20 +5,52 @@ public class Vigenere {
 	private static final String alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,. ";
 
 
-	public String finalKey(String key,String phrase) {
-		char[] newKey = new char[phrase.length()];
+	public String finalKey(String key,String word) {
+		char[] newKey = new char[word.length()];
 
-		for (int i = 0; i < phrase.length(); i++) {
+		for (int i = 0; i < word.length(); i++) {
 			newKey[i] = key.charAt(i % key.length());
 		}
 		return new String(newKey);
 	}
-//	 public String encrypyion(String key,String phrase) {
-//		 char[] temp = new char[phrase.length()];
-//		 for(int i = 0; i < phrase.length(); i++) {
-//			 int temp = 
-//		 }
-//	 }
+	public String encrypyion(String key,String word) {
+		char[] temp = new char[word.length()];
 
+		for(int i = 0; i < word.length(); i++) {
+			int temp1 = finder(word.charAt(i)) + finder(key.charAt(i));
 
+			if(temp1<alph.length()) {
+				temp[i] = alph.charAt(temp1);
+			}else {
+				temp[i] = alph.charAt(temp1 - alph.length());
+			}
+
+		}
+		return new String(temp);
+	}
+
+	public String dencrypyion(String key,String word) {
+		char[] temp = new char[word.length()];
+
+		for(int i = 0; i < word.length(); i++) {
+			int temp1 = finder(word.charAt(i)) - finder(key.charAt(i));
+
+			if(temp1<alph.length()) {
+				temp[i] = alph.charAt(temp1);
+			}else {
+				temp[i] = alph.charAt(temp1 + alph.length());
+			}
+
+		}
+		return new String(temp);
+	}
+
+	private int finder (char str) {
+		for (int i = 0; i < alph.length(); i++) {
+			if (alph.charAt(i) == str) {
+				return i;
+			}
+		}
+		return 404;
+	}
 }
